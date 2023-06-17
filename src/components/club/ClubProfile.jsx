e170ecee3d97318ae343b9c68cb568b33418c1ca
hoangtc125 <tranconghoang2k1@gmail.com>
2023-06-17 09:33:58 +0700

    ui done club profile

--- src/components/club/ClubProfile.jsx
+++ src/components/club/ClubProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Card, Image, Modal } from 'antd';
+import { Card, Image, Modal, Tag, List as Listantd } from 'antd';
 import { Avatar, AvatarGroup, Box, Button, Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
@@ -11,9 +11,10 @@ import { errorNotification } from '../../utils/notification';
 import CLUB from '../../assets/images/club.jpg'
 import WALL from '../../assets/images/club/wall.png'
 import { del, get, post } from '../../utils/request';
-import { CLUB_TYPE } from '../../utils/constant';
+import { CLUB_TYPE, COLOR_REAL } from '../../utils/constant';
 import { accountSelector } from '../../redux/selectors';
-import Login from '../auth/Login';
+import LoginPage from '../../pages/auth/LoginPage';
+import UserList from './UserList';
 
 const ClubProfile = ({ rawData }) => {
     const account = useSelector(accountSelector)
@@ -21,8 +22,9 @@ const ClubProfile = ({ rawData }) => {
     const [data, setData] = useState(rawData || {})
     const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const members = data?.groups ? data.groups.reduce((total, group) => [...total, ...group.members.map(e => e.user)], []) : []
-    const isFollow = (data?.followers || []).find(e => e.user.id == account?.id) ? true : false
+    const members = data?.groups ? data.groups.reduce((total, group) => [...total, ...group.members.map(e => ({...e.user, ...e}))], []) : []
+    const followers = (data?.followers || []).map(e => e.user)
+    const isFollow = followers.find(e => e.id == account?.id) ? true : false
 
     console.log("re-render");
 
@@ -43,6 +45,12 @@ const ClubProfile = ({ rawData }) => {
             errorNotification("Đã có lỗi xảy ra", "Hãy thử load lại", "bottomRight")
         }
     }
+
+    useEffect(() => {
+        if (account?.id) {
+            setIsModalOpen(false)
+        }
+    }, [account])
 
     useEffect(() => {
         if (!rawData) {
@@ -74,6 +82,26 @@ const ClubProfile = ({ rawData }) => {
     }
 
 
+    const handleViewGroup = (group) => {
+        Modal.info({
+            title: group.name,
+            className: "min-w-[70vw] max-w-[80vw]",
+            centered: true,
+            content: (
+                <Box className="w-full max-h-[60vh] overflow-auto flex flex-col items-center justify-center space-y-2">
+                    <Typography variant='h6'>{group.name}</Typography>
+                    <Typography variant='body1'>{group.description}</Typography>
+                    <Card title={`Danh sách thành viên (${group.members.length})`} className='w-full'>
+                        <UserList data={group.members.map(e => e.user)} />
+                    </Card>
+                </Box>
+            ),
+            onOk() { },
+            onCancel() { },
+        });
+    }
+
+
     return (
         <Box
             sx={{
@@ -98,7 +126,7 @@ const ClubProfile = ({ rawData }) => {
                 />
                 <Typography variant="h3" className='text-black uppercase'>{data?.name}</Typography>
                 <Typography variant="h6" className='text-black uppercase'>{data?.nickname}</Typography>
-                <Box className="w-full flex items-center justify-between space-x-4">
+                <Box className="w-full flex flex-col xl:flex-row items-center justify-between space-x-4">
                     <Box className="w-full flex-1"></Box>
                     <Box className="w-full flex-1 flex justify-center items-center">
                         <AvatarGroup max={4}>
@@ -108,11 +136,26 @@ const ClubProfile = ({ rawData }) => {
                                 ))
                             }
                         </AvatarGroup>
-                        <Button type='secondary' color='success' startIcon={<StarIcon />}>
+                        <Button type='secondary' color='success' startIcon={<StarIcon />}
+                            onClick={() => {
+                                Modal.info({
+                                    title: "Quan tâm",
+                                    className: "min-w-[70vw] max-w-[80vw]",
+                                    centered: true,
+                                    content: (
+                                        <Box className="w-full max-h-[60vh] overflow-auto">
+                                            <UserList data={followers} />
+                                        </Box>
+                                    ),
+                                    onOk() { },
+                                    onCancel() { },
+                                });
+                            }}
+                        >
                             Đang quan tâm
                         </Button>
                     </Box>
-                    <Box className="w-full flex-1 text-end space-x-2">
+                    <Box className="w-full flex-1 text-center xl:text-end space-x-2">
                         <Button variant="outlined" startIcon={<LoginIcon />}>
                             Xin gia nhập
                         </Button>
@@ -133,59 +176,82 @@ const ClubProfile = ({ rawData }) => {
                     </Box>
                 </Box>
             </Box>
-            <Card title="Thông tin chung" bordered={false}
+            <Card title="Giới thiệu" bordered={false}
+                className="p-8 sm:p-4 bg-white w-full shadow-md rounded-md"
+            >
+                <List className='w-full grid grid-cols-1 sm:grid-cols-2'>
+                    <ListItem className=''>
+                        {CLUB_TYPE[data?.type]}
+                    </ListItem>
+                    <ListItem className=''>
+                        <ListItemText primary="Mô tả" secondary={data?.description} />
+                    </ListItem>
+                    <ListItem className=''>
+                        <ListItemText primary="Địa chỉ Email" secondary={data?.email} />
+                    </ListItem>
+                    <ListItem className=''>
+                        <ListItemText primary="Địa chỉ" secondary={data?.address} />
+                    </ListItem>
+                    <ListItem className=''>
+                        <ListItemText primary="Slogan" secondary={data?.slogan} />
+                    </ListItem>
+                    <ListItem className=''>
+                        <ListItemText primary="Thành viên" secondary={members?.length} />
+                    </ListItem>
+                </List>
+            </Card>
+            <Card title="Cơ cấu tổ chức" bordered={false}
                 className="p-8 sm:p-4 bg-white w-full shadow-md rounded-md"
             >
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={6}>
-                        <List className='w-full grid grid-cols-1 sm:grid-cols-2'>
-                            <ListItem className='border'>
-                                <ListItemText primary="Loại hình" secondary={CLUB_TYPE[data?.type]} />
-                            </ListItem>
-                            <ListItem className='border'>
-                                <ListItemText primary="Mô tả" secondary={data?.description} />
-                            </ListItem>
-                            <ListItem className='border'>
-                                <ListItemText primary="Địa chỉ Email" secondary={data?.email} />
-                            </ListItem>
-                            <ListItem className='border'>
-                                <ListItemText primary="Địa chỉ" secondary={data?.address} />
-                            </ListItem>
-                            <ListItem className='border'>
-                                <ListItemText primary="Slogan" secondary={data?.slogan} />
-                            </ListItem>
-                            <ListItem className='border'>
-                                <ListItemText primary="Thành viên" secondary={members?.length} />
-                            </ListItem>
-                        </List>
-                    </Grid>
-                    <Grid item xs={12} md={6} className='w-full'>
-                        <List className='w-full grid grid-cols-1 sm:grid-cols-2'>
-                            <ListItem>
-                                <ListItemText primary="Loại hình" secondary={CLUB_TYPE[data?.type]} />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Mô tả" secondary={data?.description} />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Địa chỉ Email" secondary={data?.email} />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Địa chỉ" secondary={data?.address} />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Slogan" secondary={data?.slogan} />
-                            </ListItem>
-                        </List>
-                    </Grid>
+                    {
+                        (data?.groups || []).map((group, idx) => (
+                            <Grid item xs={12} md={6}
+                                key={idx}
+                            >
+                                <Tag
+                                    color={COLOR_REAL[idx % COLOR_REAL.length]}
+                                    className='w-full rounded-lg'
+                                    onClick={() => { handleViewGroup(group) }}
+                                >
+                                    <Card hoverable
+                                        bordered={true}
+                                        className='hover:cursor-pointer'
+                                        title={
+                                            <Typography className='text-base lg:text-2xl'>{group.name}</Typography>
+                                        }
+                                        extra={
+                                            <Typography>{`${group.members.length} thành viên`}</Typography>
+                                        }
+                                    >
+                                        <Card.Meta
+                                            description={
+                                                <Typography className='text-sm lg:text-base'>{group.description}</Typography>
+                                            }
+                                        />
+                                    </Card>
+                                </Tag>
+                            </Grid>
+                        ))
+                    }
                 </Grid>
             </Card>
-            <Card title="Tham gia" bordered={false}
+            <Card title={`Danh sách thành viên (${members.length})`} bordered={false}
                 className="p-8 sm:p-4 bg-white w-full shadow-md rounded-md"
             >
+                <Box className="w-full items-center max-h-[60vh] overflow-auto">
+                    <UserList data={members} />
+                </Box>
             </Card>
-            <Modal centered width={1000} open={isModalOpen} onOk={handleOk} onCancel={handleOk} destroyOnClose={true}>
-                <Login />
+            <Card title={`Người theo dõi ${followers.length}`} bordered={false}
+                className="p-8 sm:p-4 bg-white w-full shadow-md rounded-md"
+            >
+                <Box className="w-full items-center max-h-[60vh] overflow-auto">
+                    <UserList data={followers} />
+                </Box>
+            </Card>
+            <Modal centered width={1500} open={isModalOpen} onOk={handleOk} onCancel={handleOk} destroyOnClose={true}>
+                <LoginPage />
             </Modal>
         </Box>
     );
--- src/components/club/UserList.jsx
+++ src/components/club/UserList.jsx
@@ -1,46 +1,70 @@
-import { Avatar, Box } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 import { Card, List } from 'antd';
 import React from 'react';
 import VerifiedIcon from '@mui/icons-material/Verified';
+import moment from 'moment';
 
 import { getProviderIcon } from '../../utils/kind';
+import { CLUB_ROLE, MEMBERSHIP_STATUS } from '../../utils/constant';
 
-const UserList = ({data}) => {
+const UserList = ({ data }) => {
 
     return (
         <List
             dataSource={data}
-            renderItem={(account, idx) => (
-                <Card hoverable
-                    key={idx}
-                    bordered={true}
-                    className='hover:cursor-pointer m-2'
-                    onClick={() => {
-                        window.location.href = `/algo-frontend-service/account/${account.id || ""}`
-                    }}
-                >
-                    <Card.Meta
-                        avatar={
-                            <Avatar
-                                alt='avatar'
-                                src={account?.photo_url}
-                            />
-                        }
-                        title={
-                            <Box>
-                                {account.name}
-                                {account?.verify?.status ? <VerifiedIcon className='bg-white rounded-full ml-1' fontSize="small" color="primary" /> : <></>}
+            renderItem={(account, idx) => {
+                console.log(account?.role);
+                return (
+                    <Card hoverable
+                        key={idx}
+                        bordered={true}
+                        className='hover:cursor-pointer m-2'
+                        onClick={() => {
+                            window.location.href = `/algo-frontend-service/account/${account.id || ""}`
+                        }}
+                    >
+                        <Card.Meta
+                            avatar={
+                                <Avatar
+                                    alt='avatar'
+                                    src={account?.photo_url}
+                                />
+                            }
+                            title={
+                                <Box>
+                                    {account.name}
+                                    {account?.verify?.status ? <VerifiedIcon className='bg-white rounded-full ml-1' fontSize="small" color="primary" /> : <></>}
+                                </Box>
+                            }
+                            description={
+                                <Box className="w-full flex flex-col md:flex-row items-start justify-between">
+                                    {account.email}
+                                    {getProviderIcon(account?.provider)}
+                                </Box>
+                            }
+                        />
+                        {
+                            account?.role &&
+                            <Box
+                                className="flex flex-col items-start justify-start whitespace-pre-line text-base space-y-1 p-2"
+                            >
+                                <Typography variant='body2'>
+                                    {`Chức vụ: ${CLUB_ROLE[account?.role]}`}
+                                </Typography>
+                                <Typography variant='body2'>
+                                    {`Trạng thái: `}{MEMBERSHIP_STATUS[account?.status]}
+                                </Typography>
+                                <Typography variant='body2'>
+                                    {`Ngày tham gia: ${moment(account?.created_at * 1000).format('DD-MM-YYYY')}`}
+                                </Typography>
+                                <Typography variant='body2'>
+                                    {`Thế hệ: ${account?.gen || 1}`}
+                                </Typography>
                             </Box>
                         }
-                        description={
-                            <Box className="w-full flex flex-col md:flex-row items-start justify-between">
-                                {account.email}
-                                {getProviderIcon(account?.provider)}
-                            </Box>
-                        }
-                    />
-                </Card>
-            )}
+                    </Card>
+                )
+            }}
         />
     );
 }
